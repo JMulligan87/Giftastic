4f3b524d5f68d813e90037b61b8fdb447ec65e01
DobisPR <47193076+JMulligan87@users.noreply.github.com>
2019-07-06 00:29:06 -0400

    now able to pause gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@
 
 $(document).ready(function () {
     var people = ["Rodney Mullen", "Daewon Song", "Richie Jackson", "Nyjah Huston", "Luan Oliveira"];
-    
+
     function makeButtons() {
         $(".row1").empty();
         for (var i = 0; i < people.length; i++) {
@@ -22,12 +22,12 @@ $(document).ready(function () {
 
     $("#searchButton").on("click", function (event) {
         event.preventDefault();
-        var morePeople = $(".form-control").val().trim();
+        var morePeople = $(".form-control").val()
         people.push(morePeople);
         makeButtons();
         console.log(people);
     })
-    
+
     $(".buttons").on("click", function () {
         var choice = $(this).attr("data-type")
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + choice + "&api_key=4P0BC1gDEzI52ZXavK4OkasG0b4E2efG&limit=10";
@@ -36,19 +36,39 @@ $(document).ready(function () {
             method: "GET"
         })
             .then(function (response) {
-                console.log(queryURL);
-                console.log(response);
                 var results = response.data;
+                $("#gifs-go-here").empty();
                 for (var j = 0; j < results.length; j++) {
                     var personDiv = $("<div>").addClass("random m-1");
                     var personImage = $("<img>");
-                    personImage.attr("src", results[j].images.fixed_height.url);
+                    personImage.attr("src", results[j].images.fixed_height_still.url);
+                    personImage.attr("data-still", results[j].images.fixed_height_still.url);
+                    personImage.attr("data-animate", results[j].images.fixed_height.url);
+                    personImage.attr("data-state", "still");
+                    personImage.attr("class", "gif");
                     personDiv.append(personImage);
                     $("#gifs-go-here").prepend(personDiv);
 
                 }
             })
 
-    })
-   
-});
+            function pauseGifs() {
+                var dataState = $(this).attr("data-state");
+                var dataAnimate = $(this).attr("data-animate");
+                var dataStill = $(this).attr("data-still");
+                
+                if (dataState == "still") {
+                    $(this).attr("src", dataAnimate);
+                    $(this).attr("data-state", dataAnimate);
+                    
+                }else {
+                    $(this).attr("src", dataStill);
+                    $(this).attr("data-state", dataStill);
+
+                };
+
+            }
+            
+            $(document).on("click", ".gif", pauseGifs);
+        })
+    });
